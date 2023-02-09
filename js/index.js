28d989e194273fc20f0cade74640b968099c0d5c
fara003 <109841439+fara003@users.noreply.github.com>
2023-02-09 19:25:55 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let num=[0,6]
+let sum=0
+for(let item of num){
+    sum += item
+}
+
+console.log(sum);
+
+let num1=[4,2]
+let sum1=0
+for(let item of num){
+    sum1 += item
+}
+
+console.log(sum1);
+
+let num2=[1,5]
+let sum2=0
+for(let item of num){
+    sum2 += item
+}
+
+console.log(sum2);
+
+let num3=[-1,7]
+let sum3=0
+for(let item of num){
+    sum3 += item
+}
+
+console.log(sum3);
+
+let num4=[-2,8]
+let sum4=0
+for(let item of num){
+    sum4 += item
+}
+
+console.log(sum4);
